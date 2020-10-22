dcf21424f534bc92c4db318345e9e0e322a0dd42
Hannah-Scheuer <hscheuer20@gmail.com>
2020-10-21 23:00:09 -0400

    Added the methods for homework ten

--- Demo.java
+++ Demo.java
@@ -16,4 +16,53 @@ public class Demo{
       System.out.println();
     }
   }
+
+  public static String arrToString(int[]arr){
+    if (arr.length==0){
+      return "{}";
+    }
+    String total = "{";
+    for (int i=0;i<arr.length-1;i++){
+      total = total + Integer.toString(arr[i]) + ", ";
+    }
+    return total +Integer.toString(arr[arr.length-1])+"}";
+  }
+
+  public static String arrayDeepToString(int[][]arr){
+    if (arr.length==0){
+      return "{}";
+    }
+    String total = "{";
+    for (int i=0;i<arr.length-1;i++){
+      total = total + arrToString(arr[i]) + ", ";
+    }
+    return total + arrToString(arr[arr.length-1])+"}";
+  }
+
+  public static int randomNum(int max){
+    return (int) Math.round(Math.random() * max);
+  }
+
+ public static int[][] create2DArray(int rows, int cols, int maxValue){
+   int[][] hello = new int[rows][cols];
+   for (int i = 0; i<rows;i++){
+     for (int j = 0; j<cols;j++){
+       hello[i][j]= randomNum(maxValue);
+     }
+   }
+   return hello;
+ }
+
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+    int[][] arr = new int[rows][0];
+    for (int i = 0; i<rows;i++){
+      arr[i] = new int[randomNum(cols)];
+      for (int j = 0; j<arr[i].length; j++){
+        arr[i][j]= randomNum(maxValue);
+      }
+    }
+    return arr;
+  }
+
+
 }
